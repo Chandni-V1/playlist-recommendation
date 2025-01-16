df21fc65c4b0f0438d43422c3235c9d4059489fc
Chandni-V1 <chandniramrakhiyani01@gmail.com>
2025-01-16 11:01:38 +0530

    Update spotify-config.js

--- spotify-config.js
+++ spotify-config.js
@@ -1,6 +1,6 @@
 const SPOTIFY_CONFIG = {
     CLIENT_ID: '30c55e2da3b745b383fc27c1795e133c', // Replace with your Client ID
-    REDIRECT_URI: 'https://chandni-v1.github.io/playlist-recommendation/', // Replace with your hosted URL
+    REDIRECT_URI: 'https://chandni-v1.github.io/playlist-recommendation', // Replace with your hosted URL
     SCOPES: [
         'user-top-read',
         'playlist-read-private',
